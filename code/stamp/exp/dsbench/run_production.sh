81229f084e25f1627aa1052f33f079c505fbc3a4
Trevor Brown <trbot86@gmail.com>
2020-07-21 19:32:40 -0400

    started porting experimental setup to use trbot86:tools/data_framework instead of ad-hoc scripts. see exp/... - begun some experimental analysis of hytm2_3path and hytm2 inconsistent performance at high thread counts; see exp/hytm2_3p.../investigation.ipynb

---
+++ code/stamp/exp/dsbench/run_production.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../tools/data_framework/run_experiment.py exp_definition.py --production --do-plot --do-pages
---
+++ code/stamp/exp/dsbench/run_testing.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../tools/data_framework/run_experiment.py exp_definition.py --testing --do-plot --do-pages
